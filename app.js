fb64e713f48c5562a78cae99e832c457cc9ce185
endra1234567 <167106102+endra1234567@users.noreply.github.com>
2025-06-16 17:51:04 +0200

    Versioni i dyte
    Shtimi i nje GET tjeter /contact

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/about',(req,res)=>{
     res.send("i like basketball")
 })
 
+app.get('/contact',(req,res)=>{
+    res.send("+383 483 3723")
+})
+
 app.listen(3000, ()=>{
     console.log("serveri po punon me rregull")
 })
